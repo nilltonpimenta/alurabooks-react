3b8d359f50b64f9663f5b622ac0144cd32d2651e
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2025-04-01 18:41:53 -0400

    Header components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import "./App.css";
-import Logo from "./componentes/Logo/index.js";
-import perfil from "./images/perfil.svg";
-import sacola from "./images/sacola.svg";
-
-const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
-const icones = [perfil, sacola];
+import Header from "./componentes/Header";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <Logo></Logo>
-                <ul className="opcoes">
-                    {textoOpcoes.map((texto) => (
-                        <li className="opcao">{texto}</li>
-                    ))}
-                </ul>
-                <ul className="icones">
-                    {icones.map((icone) => (
-                        <li className="icone">
-                            <img src={icone}></img>
-                        </li>
-                    ))}
-                </ul>
-            </header>
+            <Header />
         </div>
     );
 }
---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,15 @@
+import "./estilo.css";
+
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
+
+function OpcoesHeader() {
+    return (
+        <ul className="opcoes">
+            {textoOpcoes.map((texto) => (
+                <li className="opcao">{texto}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default OpcoesHeader;
