b39ac08621f3b5439b96c7605155e97d0afc569e
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2025-04-03 15:59:45 -0400

    useState from Pesquisa

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livroImage from "../../images/livro.png";
+
+export const livros = [
+    { nome: "Liderança em Design", id: 1, src: livroImage },
+    { nome: "Liderança em UX", id: 2, src: livroImage },
+    { nome: "Java Assertivo", id: 3, src: livroImage },
+    { nome: "Cangaceiro em Java", id: 4, src: livroImage },
+    { nome: "Aventureiros do Javascript", id: 5, src: livroImage },
+];
