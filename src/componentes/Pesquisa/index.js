48b354f8da5df0d33b5fa6ee074050e69d3f9bc2
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2025-04-17 18:42:49 -0400

    axios

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import InputPesquisa from "../InputPesquisa/InputPesquisa";
 import styled from "styled-components";
 import { useState } from "react";
-import { livros } from "./dadosPesquisa";
+import { useEffect } from "react";
+import { getLivros } from "../../services/livros";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -47,6 +48,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        fetchLivros();
+    }, []);
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros();
+        setLivros(livrosDaAPI);
+    }
 
     return (
         <PesquisaContainer>
